30317a585ae5bb9fa5cad1e9fbccd01185c105d8
mavubsa3-dev <mavubsa3@gmail.com>
2026-07-22 17:24:12 +0900

    chore: 프로젝트 초기 세팅

--- src/main/java/com/example/demo/Team9FinalProjectApplication.java
+++ src/main/java/com/example/demo/Team9FinalProjectApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class DemoApplication {
+public class Team9FinalProjectApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(DemoApplication.class, args);
+		SpringApplication.run(Team9FinalProjectApplication.class, args);
 	}
 
 }
