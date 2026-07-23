68aa926a25d0a9995c1d05b6abda3f21be7e7bf9
Sole02 <ljaeseok02@gmail.com>
2026-07-23 15:28:42 +0900

    add: 상품 목록 조회 API에 categoryId 요청 파라미터 추가

--- src/main/java/com/example/demo/domain/product/controller/ProductController.java
+++ src/main/java/com/example/demo/domain/product/controller/ProductController.java
@@ -9,6 +9,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RequiredArgsConstructor
@@ -19,8 +20,10 @@ public class ProductController {
     private final ProductService productService;
 
     @GetMapping
-    public ResponseEntity<List<GetProductListResponse>> getProducts() {
-        return ResponseEntity.ok(productService.getProducts());
+    public ResponseEntity<List<GetProductListResponse>> getProducts(
+            @RequestParam(required = false) Long categoryId
+    ) {
+        return ResponseEntity.ok(productService.getProducts(categoryId));
     }
 
     @GetMapping("/{productId}")
