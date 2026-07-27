b78cb2c3115ee50f210dfc8c0d5a57ea3edcbc98
mavubsa3-dev <mavubsa3@gmail.com>
2026-07-27 14:11:53 +0900

    import문 정리 및 상품 상태 변경 메서드 적용

--- src/main/java/com/example/demo/domain/auth/service/AuthService.java
+++ src/main/java/com/example/demo/domain/auth/service/AuthService.java
@@ -1,7 +1,5 @@
 package com.example.demo.domain.auth.service;
 
-import com.example.demo.domain.cart.entity.Cart;
-import com.example.demo.domain.cart.repository.CartRepository;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
