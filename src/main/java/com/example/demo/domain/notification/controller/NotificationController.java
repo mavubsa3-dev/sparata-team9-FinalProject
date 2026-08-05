2ea1155a7bed5ff9a04e9a917ef99b1cb9366aad
mavubsa3-dev <mavubsa3@gmail.com>
2026-08-05 22:28:30 +0900

    import문 정리

--- src/main/java/com/example/demo/domain/notification/controller/NotificationController.java
+++ src/main/java/com/example/demo/domain/notification/controller/NotificationController.java
@@ -3,7 +3,6 @@ package com.example.demo.domain.notification.controller;
 import java.util.List;
 
 import org.springframework.http.HttpStatus;
-import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.GetMapping;
