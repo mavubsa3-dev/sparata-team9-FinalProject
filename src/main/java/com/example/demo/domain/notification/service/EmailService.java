715afac8cd8afa5b44ac64e34cc5a83f88bcbba6
mavubsa3-dev <mavubsa3@gmail.com>
2026-08-18 10:24:18 +0900

    del : 알림 기능 Consumer 서버로 이전 통한 삭제

--- src/main/java/com/example/demo/domain/notification/service/EmailService.java
+++
@@ -1,8 +0,0 @@
-package com.example.demo.domain.notification.service;
-
-import com.example.demo.domain.notification.dto.request.SendEmailMessageRequest;
-
-public interface EmailService {
-
-	void send(SendEmailMessageRequest sendEmailMessageRequest);
-}
