9c8693a45486a055463dba3ffe9e5ef52ba60327
20LDH <dhlee8461@gmail.com>
2026-08-19 11:02:48 +0900

    feat: 병합 과정 전 커밋

--- src/main/java/com/example/demo/domain/order/service/OrderService.java
+++ src/main/java/com/example/demo/domain/order/service/OrderService.java
@@ -18,6 +18,7 @@ import com.example.demo.domain.payment.service.PaymentService;
 import com.example.demo.domain.product.entity.Product;
 import com.example.demo.domain.product.entity.ProductStatus;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
